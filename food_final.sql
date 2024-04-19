466ba69069791094cb269348be163d1c4d16fde8
Vanessa Herrada Saavedra <163647765+vanessaherrada@users.noreply.github.com>
2024-04-19 19:08:04 +0200

    food_final.sql

--- food_final.sql
+++ food_final.sql
@@ -1,4 +1,4 @@
--- checking if the database has been correctly imported
+-- Checking if the database has been correctly imported
 SELECT *
 FROM food_schema.food_table;
 
@@ -6,24 +6,24 @@ FROM food_schema.food_table;
 SELECT count(*)
 from food_schema.food_table;
 
--- 76 females (gender=1) and 49 males (gender=2) participated 
+-- 76 females (gender=1) and 49 males (gender=2) participated in this study
 SELECT Gender, COUNT(Gender) 
 FROM food_schema.food_table
 GROUP BY Gender; 
 
 -- Participants with the highest weights
-/* The first 3 participants with the highest weights are men that cook only during holidays or never, one describes their diet as uncluear and the 
- * other two as healthy/balanced/moderated, 2 believe their eating changes since their were in college are better and the 3rd one thinks is the 
- * same, one of them excercises everyday another one once a week and the last one didn't answer,  one of the never checks the nutritional values 
- * and the other two on certain products only*/
+/* Among the participants with the highest weights, the top three consist of men who engage in cooking exclusively during holidays or not at all. 
+ * One individual characterizes their dietary habits as unclear, while the other two describe theirs as healthy, balanced, or moderated. 
+ * Additionally, two believe their eating habits have improved since college, while the third perceives them to be unchanged. In terms of physical 
+ * activity, one participant exercises daily, another does so weekly, and the remaining individual did not provide a response. Notably, one of the 
+ * participants never checks nutritional values, while the other two do so selectively, focusing on specific products.*/
 SELECT gender, weight, cook,  diet_current_coded, eating_changes_coded,  exercise, nutritional_check
 FROM food_schema.food_table
 WHERE weight != -1
 ORDER BY weight DESC
 LIMIT 3;
 
-/* The 66% of the students have a weight under 156 pounds, 
- * 54% between 56 and 210 pounds and 5% between 211-265 pounds */
+/* The 66% of the students weight 156 pounds, 54% between 56 and 210 pounds and 5% between 211-265 pounds */
 select 
 	case
 		when weight <= 155 then 'Group A (<=155 pounds)'
@@ -36,15 +36,16 @@ from food_schema.food_table ft
 group by weight_group
 order by weight_group;
 
--- The most common reason to eat comfort food is boredom, while the less common is hunger, excludind the NAs(-1) and the ones that answer none
+-- The most common reason to eat comfort food is boredom, while the less common is hunger, excluding the NAs(-1) and the ones that answer none.
 SELECT comfort_food_reasons_coded, COUNT(comfort_food_reasons_coded) AS Frequency
 FROM food_schema.food_table
 WHERE comfort_food_reasons_coded != -1 AND comfort_food_reasons_coded != 9
 GROUP BY comfort_food_reasons_coded
 ORDER BY Frequency DESC;
 
-/* The 48% of the total participants, which are 60 people, work part-time; while 54 participants (43%) do not work. 7% (9 participants) did not respond to 
- * the question, and only 1%, which is 2 participants, work full-time. So we can infer that */
+/* Of the total participants, comprising 125 individuals, 48% or 60 people work part-time, while 43%, equivalent to 54 participants, do not work. 
+ * A small portion, 7% or 9 participants, did not provide a response to the question. Interestingly, only 1%, represented by 2 participants, engage in 
+ * full-time employment.  */
 SELECT employment, COUNT(employment) as frequency, COUNT(employment) * 100 / (SELECT COUNT(*) from food_schema.food_table) as percentage
 FROM food_schema.food_table
 GROUP BY employment
@@ -56,15 +57,16 @@ FROM food_schema.food_table
 GROUP BY exercise
 ORDER BY frequency DESC;
 
-/* The majority (73 respondents) prefer the food cooked at home, follow by 38 participants prefering both bought at store and cooked at home and
-* only 12 participants prefer th food store bought */
+/* The majority, comprising 73 respondents, express a preference for homemade food. This is followed by 38 participants who favor a combination 
+ * of store-bought and homemade meals. Interestingly, only 12 participants indicate a preference for solely store-bought food.*/
 SELECT fav_food, COUNT(fav_food) AS frequency 
 FROM food_schema.food_table
 GROUP BY fav_food 
 ORDER BY frequency DESC;
 
 
--- The most probably food that the respondents eat is italian, followed by ethnic, and the less one is persian.
+/* Based on the responses, Italian cuisine emerges as the most commonly consumed among the participants, followed by ethnic dishes. 
+Conversely, Persian cuisine appears to be the least preferred among the respondents. */
 with ethnic_food_likeliness as (
 	select ethnic_food as likely, count(ethnic_food) as ethnic_frequency
 	from food_schema.food_table ft 
@@ -107,8 +109,7 @@ left join thai_food_likeliness on thai_food_likeliness.likely =  ethnic_food_lik
 order by ethnic_food_likeliness.likely asc ;
 
 
-/* The 32% of the students have incomes higher than 100,000 USD, representing the 
-majority of the participants*/
+/* The 32% of the students have incomes higher than 100,000 USD, representing the majority of the participants*/
 select income, count(income) as frequency, count(income) * 100 / (select count(*) 
 from food_schema.food_table) as percentage
 from food_schema.food_table ft 
@@ -116,7 +117,9 @@ where income != -1
 group by income
 order by frequency desc;
 
-/* They prefer to eat out 1-2 times per week with the 60% of frequency*/
+/* The majority, accounting for 60% of the respondents, prefer to dine out 1-2 times per week. Following this, 
+ * 24% or 19 students opt for dining out 2-3 times weekly, while 12% never choose this option. Additionally, 
+ * 13 respondents, representing a minority, dine out from 3 to 5 times per week, whereas 9% do so every day.*/
 select eating_out , count(eating_out) as frequency, count(eating_out) * 100 / 
 (select count(*) from food_schema.food_table) as percentage
 from food_schema.food_table ft 
@@ -124,13 +127,11 @@ where eating_out != -1
 group by eating_out 
 order by frequency desc;
 	
-/* Most of the participants, 45%, perceive their weight as just right, 31% as slightly overweight, 
- * the same percentage feel very fit, while 6% feel slim,  and another 6% feel overweight. 
- * Only 5% don't think of themselves in these terms. */
+/* Most of the participants, comprising 45%, perceive their weight as just right. Meanwhile, 31% view themselves as slightly 
+ * overweight, with an equal percentage feeling very fit. Interestingly, 6% consider themselves slim, while another 6% feel 
+ * overweight. Only a small proportion, 5%, do not categorize themselves using these terms. */
 select self_perception_weight, count(self_perception_weight)
 from food_schema.food_table ft 
 where self_perception_weight != -1
 group by self_perception_weight
 order by count(self_perception_weight) desc ;
-
-
